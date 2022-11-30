f3e9a66bcbfaebfe6567213aa2edfa19cb0ec13c
patrick9577 <57570834+patrick9577@users.noreply.github.com>
2022-11-30 21:04:24 +0800

    first version
    first version

---
+++ src/main/java/Client/Main.java
@@ -0,0 +1,133 @@
+package Client;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.json.JSONObject;
+
+public class Main {
+	
+	private static SimpleDateFormat df_yyyyMMdd = new SimpleDateFormat("yyyyMMdd");
+	private static SimpleDateFormat df_kkmmss = new SimpleDateFormat("kkmmss");
+	private static SimpleDateFormat df_yyyyMMddkkmmss = new SimpleDateFormat("yyyyMMddkkmmss");
+	private static JSONObject response = null;
+	
+	public static void main(String args[]) throws Exception {
+		JSONObject obj = new JSONObject();
+		obj.put("activity", "DataStreaming");
+		obj.put("market", "Future");
+		obj.put("index", "YM");
+		obj.put("startdate", "20210701");
+		obj.put("enddate", "20210701");
+		obj.put("starttime", "120000");
+		obj.put("endtime", "130000");
+		obj.put("interval", "59");
+		
+		SocketClient sc = new SocketClient("funganything@gmail.com", "123");
+		//sc.request("13.250.98.96", "8888", obj);
+		sc.request("localhost", "8888", obj);
+		
+		
+		while(true) {
+			response = sc.getResponse();
+			if(response==null) 
+				break;
+			if(!response.isEmpty()) {
+				System.out.println( 
+					String.format("%s,%s,%s,%s,%s,%s,%s,%s",
+						getDateTime(response),
+						getIndex(response),
+						getVolumn(response),
+						getOpen(response),
+						getHigh(response),
+						getLow(response),
+						getClose(response),
+						getTotalVolumn(response)
+					) 
+				);
+				
+			}
+		}
+		
+	}
+	
+	public static Date getDate(JSONObject JSONresponse) throws Exception {
+		if( JSONresponse!=null && JSONresponse.has("date") ) {
+			return df_yyyyMMdd.parse(JSONresponse.getString("date"));
+		}else {
+			return null;
+		}
+	}
+	
+	public static Date getTime(JSONObject JSONresponse) throws Exception {
+		if( JSONresponse!=null && JSONresponse.has("time") ) {
+			return df_kkmmss.parse(JSONresponse.getString("time"));
+		}else {
+			return null;
+		}
+	}
+	
+	public static Date getDateTime(JSONObject JSONresponse) throws Exception {
+		if( JSONresponse!=null && JSONresponse.has("datetime") ) {
+			return df_yyyyMMddkkmmss.parse(JSONresponse.getString("datetime"));
+		}else {
+			return null;
+		}
+	}
+	
+	public static float getIndex(JSONObject JSONresponse) throws Exception {
+		if( JSONresponse!=null && JSONresponse.has("index") ) {
+			return Float.parseFloat(JSONresponse.getString("index"));
+		}else {
+			return -1;
+		}
+	}
+	
+	public static int getVolumn(JSONObject JSONresponse) throws Exception {
+		if( JSONresponse!=null && JSONresponse.has("volumn") ) {
+			return Integer.parseInt(JSONresponse.getString("volumn"));
+		}else {
+			return -1;
+		}
+	}
+	
+	public static float getOpen(JSONObject JSONresponse) throws Exception {
+		if( JSONresponse!=null && JSONresponse.has("open") ) {
+			return Float.parseFloat(JSONresponse.getString("open"));
+		}else {
+			return -1;
+		}
+	}
+	
+	public static float getHigh(JSONObject JSONresponse) throws Exception {
+		if( JSONresponse!=null && JSONresponse.has("high") ) {
+			return Float.parseFloat(JSONresponse.getString("high"));
+		}else {
+			return -1;
+		}
+	}
+	
+	public static float getLow(JSONObject JSONresponse) throws Exception {
+		if( JSONresponse!=null && JSONresponse.has("low") ) {
+			return Float.parseFloat(JSONresponse.getString("low"));
+		}else {
+			return -1;
+		}
+	}
+	
+	public static float getClose(JSONObject JSONresponse) throws Exception {
+		if( JSONresponse!=null && JSONresponse.has("close") ) {
+			return Float.parseFloat(JSONresponse.getString("close"));
+		}else {
+			return -1;
+		}
+	}
+	
+	public static int getTotalVolumn(JSONObject JSONresponse) throws Exception {
+		if( JSONresponse!=null && JSONresponse.has("total volumn") ) {
+			return Integer.parseInt(JSONresponse.getString("total volumn"));
+		}else {
+			return -1;
+		}
+	}
+}
---
+++ src/main/java/Client/SocketClient.java
@@ -0,0 +1,140 @@
+package Client;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+
+public class SocketClient implements Runnable {
+
+	private JSONObject JSONrequest;
+	private String serverIPaddress;
+	private int serverPort;
+	private Socket client;
+	private BufferedWriter bw;
+	private String messageFromServer;
+	private BufferedReader br;
+	private String apiAccessCode;
+	private String clientID;
+	private ArrayList<JSONObject> JSONresponse = new ArrayList<JSONObject>();
+	
+	public SocketClient(String loginname, String password) throws Exception {
+		JSONObject obj = new JSONObject();
+		obj.put("clientID", loginname);
+		obj.put("password", password);
+		JSONObject result = postRequest("https://www.projectb.click/ProjectB/APIgetAccessCode.php", obj.toString());
+		if(result.has("type") && result.getString("type").equals("error")) {
+			throw new Exception(result.getString("message"));
+		}else {
+			this.clientID = result.getString("clientID");
+			this.apiAccessCode = result.getString("accessCode");
+		}
+	}
+	
+	public void request(String serverIPaddress, String serverPort, JSONObject request) throws Exception {
+		if(clientID==null || apiAccessCode==null) {
+			throw new Exception("No available API access code");
+		}else {
+			this.serverIPaddress = serverIPaddress;
+			this.serverPort = Integer.parseInt(serverPort);
+			this.JSONrequest = request;
+			JSONrequest.put("clientID", clientID);
+			JSONrequest.put("accessCode", apiAccessCode);
+			//Run program
+			Thread thread = new Thread(this);
+			thread.start();
+		}
+	}
+	
+	@Override
+	public void run() {
+		try {
+			//connect to server
+			client = new Socket(serverIPaddress, serverPort);
+			
+			//send request from server
+			InputStream is = client.getInputStream();
+			OutputStream os = client.getOutputStream();
+			bw = new BufferedWriter(new OutputStreamWriter(os));
+			bw.write(JSONrequest.toString());
+			bw.flush();
+			
+			//get response from server
+			JSONresponse = new ArrayList<JSONObject>();
+			while(true) {
+				br = new BufferedReader(new InputStreamReader(is));
+				while((messageFromServer = br.readLine())!=null) {
+					if(messageFromServer.contains("done")) {
+						JSONresponse.add(null);
+						break; 
+					}
+					JSONresponse.add(new JSONObject(messageFromServer));
+				}
+			}
+		} catch (java.io.IOException e) {
+			System.out.println("Socket連線有問題 !");
+			System.out.println("IOException :" + e.toString());
+		} catch (Exception e) {
+			System.out.println("Exception :" + e.toString());
+		} 
+		finally {
+			try {
+				bw.close();
+				client.close();
+			} catch (IOException e) {}
+		}
+	}
+	
+	private JSONObject postRequest(String url, String message) throws IOException {
+		URL obj = new URL(url);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) obj.openConnection();
+        httpURLConnection.setRequestMethod("POST");
+        httpURLConnection.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        // For POST only - START
+        httpURLConnection.setDoOutput(true);
+        OutputStream os = httpURLConnection.getOutputStream();
+        os.write(message.getBytes());
+        os.flush();
+        os.close();
+        // For POST only - END
+
+        int responseCode = httpURLConnection.getResponseCode();
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in .readLine()) != null) {
+                response.append(inputLine);
+            } in .close();
+
+            // print result
+            return new JSONObject(response.toString());
+        } else {
+            System.out.println("POST request not worked");
+        }
+        
+        return null;
+	}
+	
+	public JSONObject getResponse() {
+		if(JSONresponse.size()>0) {
+			JSONObject temp = JSONresponse.get(0);
+			JSONresponse.remove(0);
+			return temp;
+		}
+		else{
+			return new JSONObject();
+		}
+	}
+}

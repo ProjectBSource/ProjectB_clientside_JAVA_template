4d8752f57347e666a304aee74f46dfc0a33a7624
PatrickFung <patrick.fung@team-c.com>
2023-03-22 17:54:13 +0800

    enhancement

---
+++ src/main/java/Client/DataStructure.java
@@ -0,0 +1,117 @@
+package Client;
+
+public class DataStructure {
+	String runJobID = null;
+	String type = null;
+	String symbol = null;
+	String market = null;
+	String date = null;
+	String time = null;
+	String datetime = null;
+	String index = null;
+	String open = null;
+	String high = null;
+	String low = null;
+	String close = null;
+	String volumn = null;
+	String total_volumn = null;
+	String error = null;
+	String done = null;
+	
+	public String getRunJobID() {
+		return runJobID;
+	}
+	public void setRunJobID(String runJobID) {
+		this.runJobID = runJobID;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public String getMarket() {
+		return market;
+	}
+	public void setMarket(String market) {
+		this.market = market;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public String getDatetime() {
+		return datetime;
+	}
+	public void setDatetime(String datetime) {
+		this.datetime = datetime;
+	}
+	public String getIndex() {
+		return index;
+	}
+	public void setIndex(String index) {
+		this.index = index;
+	}
+	public String getOpen() {
+		return open;
+	}
+	public void setOpen(String open) {
+		this.open = open;
+	}
+	public String getHigh() {
+		return high;
+	}
+	public void setHigh(String high) {
+		this.high = high;
+	}
+	public String getLow() {
+		return low;
+	}
+	public void setLow(String low) {
+		this.low = low;
+	}
+	public String getClose() {
+		return close;
+	}
+	public void setClose(String close) {
+		this.close = close;
+	}
+	public String getVolumn() {
+		return volumn;
+	}
+	public void setVolumn(String volumn) {
+		this.volumn = volumn;
+	}
+	public String getTotal_volumn() {
+		return total_volumn;
+	}
+	public void setTotal_volumn(String total_volumn) {
+		this.total_volumn = total_volumn;
+	}
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	public String getDone() {
+		return done;
+	}
+	public void setDone(String done) {
+		this.done = done;
+	}
+}

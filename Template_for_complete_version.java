41310876298d421e8c32d7ba08108f00d566074d
ProjectB <57570834+ProjectBSource@users.noreply.github.com>
2023-08-09 14:47:49 +0800

    Update Template_for_complete_version.java

--- Template_for_complete_version.java
+++ Template_for_complete_version.java
@@ -8,7 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 
 public class Main {
-		
+        
     //Initial the ObjectMapper
     public static ObjectMapper mapper = new ObjectMapper();
     //Initial the JSONObject 
@@ -16,68 +16,68 @@ public class Main {
     //Initial the DataStructure
     public static DataStructure dataStructure;
 
-	  public static void main(String args[]) throws Exception {
-		
-  		//Login here
-  		SocketClient dataStreaming = new SocketClient(/*email*/, /*password*/);
-  		
-  		//Form JSON object message for data streaming request
-  		JSONObject dataStreamingRequest = new JSONObject();
-  		dataStreamingRequest.put("activity", "@#activity#@");
-  		dataStreamingRequest.put("market", "@#market#@");
-  		dataStreamingRequest.put("index", "@#index#@");
-  		dataStreamingRequest.put("startdate", "@#startdate#@");
-  		dataStreamingRequest.put("enddate", "@#enddate#@");
-  		dataStreamingRequest.put("starttime", "@#starttime#@");
-  		dataStreamingRequest.put("endtime", "@#endtime#@");
-  		dataStreamingRequest.put("interval", @#interval#@-1);
-      	dataStreamingRequest.put("mitigateNoiseWithinPrecentage", @#mitigateNoiseWithinPrecentage#@);
-  		
-  		//Send the request to server
-  		dataStreaming.request(dataStreamingRequest);
-  		
-  		/*
-  		 * This template included a simple account and order management function
-  		 * You may modify the function to fit your back test
-  		 */
-  		TradeController tradeController = new TradeController();
-  		tradeController.setSlippage(@#slippage#@);
-  		
-      	//Setup the indicatories you need here
-      	@#indicatories#@
-  		
-  		while(true) {
-			//get the response
-			response = dataStreaming.getResponse();
-			if(!response.isEmpty()) {
-    				System.out.flush();
-    				//Convert response JSON message to Java class object
-    				DataStructure dataStructure = mapper.readValue(response.toString(), DataStructure.class);
-    				//Check response finished or not
-    				if(dataStructure.getDone()!=null) {
-    					break;
-    				}
-    				//Check error caused or not
-    				if(dataStructure.getError()!=null) {
-    					System.out.println(dataStructure.getError());
-    					break;
-    				}
-    				//check the order allow to trade or not
-    				tradeController.tradeCheckingAndBalanceUpdate(dataStructure);
-    				
-    				/*
-    				 * You may write your back test program below within the while loop
-    				 * >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    				 */
+      public static void main(String args[]) throws Exception {
+        
+          //Login here
+          SocketClient dataStreaming = new SocketClient(/*email*/, /*password*/);
+          
+          //Form JSON object message for data streaming request
+          JSONObject dataStreamingRequest = new JSONObject();
+          dataStreamingRequest.put("activity", "@#activity#@");
+          dataStreamingRequest.put("market", "@#market#@");
+          dataStreamingRequest.put("index", "@#index#@");
+          dataStreamingRequest.put("startdate", "@#startdate#@");
+          dataStreamingRequest.put("enddate", "@#enddate#@");
+          dataStreamingRequest.put("starttime", "@#starttime#@");
+          dataStreamingRequest.put("endtime", "@#endtime#@");
+          dataStreamingRequest.put("interval", @#interval#@-1);
+          dataStreamingRequest.put("mitigateNoiseWithinPrecentage", @#mitigateNoiseWithinPrecentage#@);
+          
+          //Send the request to server
+          dataStreaming.request(dataStreamingRequest);
+          
+          /*
+           * This template included a simple account and order management function
+           * You may modify the function to fit your back test
+           */
+          TradeController tradeController = new TradeController();
+          tradeController.setSlippage(@#slippage#@);
+          
+          //Setup the indicatories you need here
+          @#indicatories#@
+          
+          while(true) {
+            //get the response
+            response = dataStreaming.getResponse();
+            if(!response.isEmpty()) {
+                    System.out.flush();
+                    //Convert response JSON message to Java class object
+                    DataStructure dataStructure = mapper.readValue(response.toString(), DataStructure.class);
+                    //Check response finished or not
+                    if(dataStructure.getDone()!=null) {
+                        break;
+                    }
+                    //Check error caused or not
+                    if(dataStructure.getError()!=null) {
+                        System.out.println(dataStructure.getError());
+                        break;
+                    }
+                    //check the order allow to trade or not
+                    tradeController.tradeCheckingAndBalanceUpdate(dataStructure);
+                    
+                    /*
+                     * You may write your back test program below within the while loop
+                     * >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+                     */
     
-           		 		@#indicatoriesUpdateLogic#@
+                     @#indicatoriesUpdateLogic#@
     
-            			@#actionAndTradeLogic#@
+                     @#actionAndTradeLogic#@
     
-    				/*
-    				 * <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-    				 */
-    			}
-    		}
-	    }
+                    /*
+                     * <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                     */
+                }
+            }
+        }
 }

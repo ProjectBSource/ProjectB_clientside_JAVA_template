514545303d00651553f23e6b4eed36153e88d75d
ProjectB <57570834+ProjectBSource@users.noreply.github.com>
2023-10-18 15:04:54 +0800

    remove the CPU checking

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -43,7 +43,6 @@ public class Main {
             WebVersionJobConstants.setWebVersionJobIPaddress();
             WebVersionJobConstants.setWebVersionJobScreenTaskID();
             WebVersionJobConstants.setWebVersionJobRunJobTaskID();
-            WebVersionJobConstants.setWebVersionJobCPUUsage();
             //setup Database communication
             WebVersionJobConstants.setupDBconnection();
             WebVersionJobConstants.initialIndicator();

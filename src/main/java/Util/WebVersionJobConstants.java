ab660d613b124c6094e14074c5457a1b9022d946
ProjectB <57570834+ProjectBSource@users.noreply.github.com>
2023-10-18 17:18:40 +0800

    Wrong bugfix, restore

--- src/main/java/Util/WebVersionJobConstants.java
+++ src/main/java/Util/WebVersionJobConstants.java
@@ -269,7 +269,7 @@ public class WebVersionJobConstants {
 						boolean indicatorFunctionExist = false;
 						Indicator tempIndicator = null;
 						for(Indicator i : indicators){
-							if(i.indicatorName.equals("BollingerBands")){
+							if(i.indicatorName.equals("Bollinger Bands")){
 								indicatorFunctionExist=true; 
 								tempIndicator = i;
 								break; 

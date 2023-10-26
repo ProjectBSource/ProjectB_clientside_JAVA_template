47129d0c45b7cf6a4da2fd8396263af5a9c4f52a
ProjectB <57570834+ProjectBSource@users.noreply.github.com>
2023-10-26 17:10:56 +0800

    Update Main.java

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -96,7 +96,7 @@ public class Main {
                     if(node.has("subscribedDataList")){
                         if(node.has("data")){
                             JSONObject data = new JSONObject(node.getString("data"));
-                            if(data.has("slippagePrecentage")==false) { 
+                            if(data.has("slippagePrecentage")==true) { 
                                 tradeController = new TradeController();
                                 tradeController.setSlippage( Double.parseDouble(data.getString("slippagePrecentage")) );
                                 WebVersionJobConstants.logger("slippagePrecentage :" + dataStreamingRequest.getDouble("slippagePrecentage"));

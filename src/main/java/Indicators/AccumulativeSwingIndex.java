15cec342cc94998effc5a23d99072d794c63c75a
ProjectB <57570834+ProjectBSource@users.noreply.github.com>
2023-11-14 11:13:08 +0800

    Add files via upload

---
+++ src/main/java/Indicators/AccumulativeSwingIndex.java
@@ -0,0 +1,60 @@
+package Indicators;
+
+public class AccumulativeSwingIndex extends Indicator{
+
+    private List<Double> highs;
+    private List<Double> lows;
+    private List<Double> closes;
+
+    private int period;
+
+    public AccumulativeSwingIndex(){
+        super.indicatorName  = "Accumulative Swing Index";
+        super.parametersAmount = 1;
+    }
+
+    public AccumulativeSwingIndex(int period){
+        super.indicatorName  = "Accumulative Swing Index";
+        super.parametersAmount = 1;
+        this.highs = new ArrayList<>();
+        this.lows = new ArrayList<>();
+        this.closes = new ArrayList<>();
+        this.period = period;
+    }
+
+    public void update(DataStructure dataStructure){
+        super.dataStructure = dataStructure;
+        highs.add(dataStructure.getHigh());
+        lows.add(dataStructure.getLow());
+        closes.add(dataStructure.getClose());
+        if (closes.size() > period) {
+            highs.remove(0);
+            lows.remove(0);
+            closes.remove(0);
+        }
+    }
+
+    public double getASI() {
+        int dataLength = highs.size();
+        double[] asiValues = new double[dataLength];
+
+        if(dataLength==0) 
+            return 0;
+        
+        // Calculate ASI values
+        for (int i = 0; i < dataLength; i++) {
+            double tr = Math.max(highs.get(i) - closes.get(i - 1), Math.max(highs.get(i) - lows.get(i), lows.get(i) - closes.get(i - 1)));
+            double r = Math.abs(closes.get(i) - closes.get(i - 1));
+            double k = Math.max(highs.get(i) - closes.get(i - 1), lows.get(i) - closes.get(i - 1));
+            double si = 50 * ((closes.get(i) - closes.get(i - 1)) + 0.5 * (closes.get(i) - lows.get(i - 1)) + 0.25 * (closes.get(i - 1) - highs.get(i - 1))) * r / (tr * k);
+            if (i > 0) {
+                asiValues[i] = asiValues[i - 1] + si;
+            } else {
+                asiValues[i] = si;
+            }
+        }
+        
+        // Return the last ASI value
+        return asiValues[dataLength - 1];
+    }
+}
---
+++ src/main/java/Indicators/AdvanceORDecline.java
@@ -0,0 +1,84 @@
+package Indicators;
+
+public class AdvanceORDecline extends Indicator{
+
+    private List<Double> advances ;
+    private List<Double> declines ;
+    private List<Double> closes;
+
+    private int period;
+
+    public AdvanceORDecline(){
+        super.indicatorName  = "Advance/Decline";
+        super.parametersAmount = 1;
+    }
+
+    public AdvanceORDecline(int period){
+        super.indicatorName  = "Advance/Decline";
+        super.parametersAmount = 1;
+        this.advances = new ArrayList<>();
+        this.declines = new ArrayList<>();
+        this.closes = new ArrayList<>();
+        this.period = period;
+    }
+
+    public void update(DataStructure dataStructure){
+        super.dataStructure = dataStructure;
+        closes.add(dataStructure.getClose());
+        if(closes.size()>1){
+            if(closes.get(closes.size()-1) - closes.get(closes.size()-2) > 0){
+                advances.add(1);
+                declines.add(0);
+            }
+            else if(closes.get(closes.size()-1) - closes.get(closes.size()-2) == 0){
+                advances.add(0);
+                declines.add(0);
+            }else{
+                advances.add(0);
+                declines.add(1);
+            }
+        }
+        if (closes.size() > period) {
+            advances.remove(0);
+            declines.remove(0);
+            closes.remove(0);
+        }
+    }
+
+    public double getAdvance(){
+        if(advances.size()>0){
+            return advances.get(advances.size()-1);
+        }else{
+            return 0;
+        }
+    }
+
+    public double getDecline(){
+        if(declines.size()>0){
+            return declines.get(declines.size()-1);
+        }else{
+            return 0;
+        }
+    }
+
+    public double getADRatio() {
+        int dataLength = advances.size();
+        double totalAdvances = 0;
+        double totalDeclines = 0;
+        
+        if(dataLength==0) 
+            return 0;
+
+        // Calculate total advances and declines
+        for (int i = 0; i < dataLength; i++) {
+            totalAdvances += advances.get(i);
+            totalDeclines += declines.get(i);
+        }
+        
+        // Calculate A/D ratio
+        double adr = totalAdvances / totalDeclines;
+        
+        // Return the A/D ratio
+        return adr;
+    }
+}

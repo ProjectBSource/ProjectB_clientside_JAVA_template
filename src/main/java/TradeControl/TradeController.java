4d31767567d5d4f9a821a8b200742e900256bb2c
ProjectB <57570834+ProjectBSource@users.noreply.github.com>
2023-11-16 16:02:28 +0800

    Update TradeController.java

--- src/main/java/TradeControl/TradeController.java
+++ src/main/java/TradeControl/TradeController.java
@@ -138,13 +138,13 @@ public class TradeController {
 	public JSONObject getOrderHistoryInJSON() throws JsonProcessingException, JSONException {
 		JSONArray history = new JSONArray();
         for(Order order : completedOrders){
-			for(Order childOrder : order.history){
-				history.put(childOrder.orderDetailInJSON);
+			for(JSONObject childOrderInJSON : order.historyInJSON){
+				history.put(childOrderInJSON);
 			}
         }
         for (Map.Entry<String, Order> order : orders.entrySet()) {
-			for(Order childOrder : order.getValue().history){
-				history.put(childOrder.orderDetailInJSON);
+			for(JSONObject childOrderInJSON : order.getValue().historyInJSON){
+				history.put(childOrderInJSON);
 			}
         }
 		JSONObject result = new JSONObject();

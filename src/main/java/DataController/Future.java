e15fa707885ca155cbb176d367c9e54be5952506
ProjectB <57570834+ProjectBSource@users.noreply.github.com>
2023-12-07 17:24:01 +0800

    Update Future.java

--- src/main/java/DataController/Future.java
+++ src/main/java/DataController/Future.java
@@ -188,7 +188,7 @@ public class Future implements Runnable {
 										Double newIndex = Double.parseDouble(sbArray[2]);
 										
 										//skip if the index defined as noise
-										boolean noise = false;
+										boolean noise = true;
 										if(mitigateNoiseWithPrecentage>-1) {
 											if(previousDataDetail!=null) {
 												Double prevIndex = previousDataDetail.getDouble("index");

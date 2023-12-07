92d76acbeb5bde01c80ef019cbdf27f55c74f4a2
ProjectB <57570834+ProjectBSource@users.noreply.github.com>
2023-12-07 14:26:42 +0800

    Create IntervalData.java

---
+++ src/main/java/DataController/IntervalData.java
@@ -0,0 +1,11 @@
+class IntervalData{
+	public String data_date_within_interval = null;
+	public String data_time_within_interval = null;
+	public String data_datetime_within_interval = null;
+	public String data_index_within_interval = null;
+	public String data_open_within_interval = null;
+	public String data_high_within_interval = null;
+	public String data_low_within_interval = null; 
+	public String data_close_within_interval = null;
+	public String data_sumupvolume_within_interval = null;
+}

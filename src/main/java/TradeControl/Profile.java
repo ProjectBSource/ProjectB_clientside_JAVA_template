9e2f44901072c576ba3f845dc59317c4fa1a9f3f
ProjectB <57570834+ProjectBSource@users.noreply.github.com>
2023-12-11 14:11:41 +0800

    Update Profile.java

--- src/main/java/TradeControl/Profile.java
+++ src/main/java/TradeControl/Profile.java
@@ -6,6 +6,7 @@ class Profile {
 	public HashMap<String, Integer> holding = new HashMap<>();
 	public double profits;
 	public double cash;
+	public ArrayList<String> historyInJSON = new ArrayList<>();
 	
 	public void update(String symbol, int quantity, double price) {
 		if(holding.containsKey(symbol)) {  
@@ -15,5 +16,14 @@ class Profile {
 			holding.put(symbol, quantity);
 		}
 		cash -= (quantity * price);
+	}
+
+	public void addHistoryNode(DataStructure data){
+		JSONObject profileDetailInJSON = new JSONObject();
+		orderDetailInJSON.put("orderDateTime", this.orderDateTime);
+		profileDetailInJSON.put("holding", this.holding);
+		profileDetailInJSON.put("profits", this.profits);
+		profileDetailInJSON.put("cash", this.cash);
+		historyInJSON.add(profileDetailInJSON.toString());
 	}
 }

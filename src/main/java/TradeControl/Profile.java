aadfde02ac47a0a43cd5b438323167aec42e5fa5
ProjectB <57570834+ProjectBSource@users.noreply.github.com>
2023-12-27 15:55:55 +0800

    Update Profile.java

--- src/main/java/TradeControl/Profile.java
+++ src/main/java/TradeControl/Profile.java
@@ -7,10 +7,13 @@ import java.util.Map;
 import org.json.JSONObject;
 
 import ClientSocketControl.DataStructure;
+import TradeControl.OrderActionConstants.Action;
 
 class Profile {
 	public HashMap<String, Integer> holding = new HashMap<>();
-	public double profits;
+	public HashMap<String, Double> orderID_tradePrice = new HashMap<>();
+	public HashMap<String, Double> orderID_profits = new HashMap<>();
+	public HashMap<String, Double> symbol_accumProfits = new HashMap<>();
 	public ArrayList<String> historyInJSON = new ArrayList<>();
 
 	public Profile(){
@@ -20,27 +23,24 @@ class Profile {
 		historyInJSON.add(profileDetailInJSON.toString());
 	}
 	
-	public void update(String symbol, int quantity, double price, DataStructure ds) {
+	public void update(String symbol, int temp_trade_amount, double price, Order order) {
 		if(holding.containsKey(symbol)) {  
-			holding.put(symbol, holding.get(symbol)+quantity);
+			holding.put(symbol, holding.get(symbol)+temp_trade_amount);
 		}
 		else {
-			holding.put(symbol, quantity);
+			holding.put(symbol, temp_trade_amount);
 		}
 
-		//update profile profits
-		profits = 0;
-		for (Map.Entry<String, Integer> item : holding.entrySet()) {
-			if(item.getKey().contains("-")){
-				if(item.getKey().equals(ds.getSymbol()+"-"+ds.getDirection()+"-"+ds.getStrike_price())) {
-					profits += item.getValue() * ds.getIndex();
-				}
+		offTrade = order.orderAlias.contains("OFF");
+		if(offTrade==false){
+			tradePrice.put(order.orderid, order.tradePrice);
+			if(symbol_accumProfits.containsKey(order.symbol)==false) {  
+				symbol_accumProfits.put(order.symbol, 0.0);
 			}
-			else{
-				if(item.getKey().equals(ds.getSymbol())) {
-					profits += item.getValue() * ds.getIndex();
-				}
-			}
+		}else{
+			tempProfits = (order.action==Action.SELL?(order.tradePrice - tradePrice.get(order.targetOffTradeId)):(tradePrice.get(order.targetOffTradeId) - order.tradePrice));
+			profits.put(order.targetOffTradeId, tempProfits );
+			symbol_accumProfits.put(order.symbol, symbol_accumProfits.get(order.symbol)+tempProfits );
 		}
 	}
 
@@ -48,7 +48,8 @@ class Profile {
 		JSONObject profileDetailInJSON = new JSONObject();
 		profileDetailInJSON.put("orderDateTime", data.getDatetime());
 		profileDetailInJSON.put("holding", this.holding);
-		profileDetailInJSON.put("profits", this.profits);
+		profileDetailInJSON.put("accum.Profits", this.symbol_accumProfits);
+		profileDetailInJSON.put("profits", this.orderID_profits);
 		historyInJSON.add(profileDetailInJSON.toString());
 	}
 }
